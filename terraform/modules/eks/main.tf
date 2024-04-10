3238f18d41847483236baf6ce980337420f04056
pandrade89 <pandrade@dealersocket.com>
2024-04-10 18:41:51 +0200

    Adding jenkins and ansible files

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -786,7 +786,7 @@ resource "aws_lambda_function" "lambda_up" {
   filename         = "${path.module}/lambda/lambda-scale.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda/lambda-scale.zip")
 
-  nvironment {
+  environment {
     variables = {
       CLUSTER_NAME  = var.cluster_name
       NODEGROUP_NAME = var.managed_node_groups.name
