cd01ea2ff833feadd6d1e54fbc302dac0ba53d2b
pandrade89 <pandrade@dealersocket.com>
2024-04-10 11:21:41 +0200

    Adding jenkins and ansible files

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -323,6 +323,11 @@ resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_asg_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+}
+
 # VPC CNI Plugin Role
 data "aws_iam_policy_document" "vpc_cni_assume_role_policy" {
   statement {
